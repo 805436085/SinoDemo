b91f2ef25cde370e9783515ca13f392e29d3080a
spoonzhang <805436085@qq.com>
2023-08-29 12:15:29 +0800

    init

---
+++ Source/SinoDemo/SinoDemo.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SinoDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SinoDemo, "SinoDemo" );
+
+DEFINE_LOG_CATEGORY(LogSinoDemo)
+ 
---
+++ Source/SinoDemo/SinoDemoGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SinoDemoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASinoDemoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASinoDemoGameMode();
+};
+
+
+
